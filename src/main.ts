9e9e9c8e7c18dc09f29bdc22ee93b665a615667d
Jose María López González <jlopgon0807b@cpifpalanturing.es>
2025-10-06 13:00:48 +0200

    paginas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { catchError, of, switchMap } from 'rxjs';
+import { BehaviorSubject, catchError, of, switchMap } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import './style.css'
 
+const btnsContainer = document.querySelector(".btns") as HTMLElement
 
 interface Personaje{
   id: number;
@@ -10,34 +11,9 @@ interface Personaje{
   maxKi: string;
   image: string;
 }
+const paginaActual = new BehaviorSubject(1);
 
-const personajes$ = fromFetch('https://dragonball-api.com/api/characters').pipe(
-  switchMap(response =>{
-    if(response.ok){
-      return response.json();
-    }else{
-      throw new Error('Error en la llamada a la API');
-    }
-  }),
-  catchError(err=>{
-    return of([{Error: true, message: err.message}]);
-  })
-)
 
-personajes$.subscribe(data=>{
-  if("Error"in data){
-    console.error(data.Error);
-  }else{
-    pintarPersonajes(data.items.map((p:Personaje) => ({
-      id: p.id,
-      name: p.name,
-      ki: p.ki,
-      maxKi: p.maxKi,
-      image: p.image
-    })));
-  
-  }
-})
 
 function pintarPersonajes(personajes: Personaje[]){
 const app = document.querySelector<HTMLDivElement>("#app")!;
@@ -53,4 +29,37 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
       <div class="ki-text">Ki: ${data.ki} / ${data.maxKi}</div>
     </div>
   `).join('');
-} 
+}
+
+btnsContainer.addEventListener("click",(event)=>{
+  const target = event.target as HTMLElement
+  if(target.classList.contains("btn")){
+   paginaActual.next(parseInt(target.textContent!))
+  }
+})
+
+paginaActual.subscribe({
+  next: (value) => fromFetch('https://dragonball-api.com/api/characters?page=' + value+"&limit=8").pipe(
+    switchMap(response =>{
+      if(response.ok){
+        return response.json();
+      }else{
+        throw new Error('Error en la llamada a la API');
+      }
+    }),
+    catchError(err=>{
+      return of([{Error: true, message: err.message}]);
+    })).subscribe(data=>{
+      if("Error"in data){
+        console.error(data.Error);
+      }else{
+        pintarPersonajes(data.items.map((p:Personaje) => ({
+          id: p.id,
+          name: p.name,
+          ki: p.ki,
+          maxKi: p.maxKi,
+          image: p.image
+        })));
+      }
+    })
+})
